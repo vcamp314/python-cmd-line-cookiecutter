c5a44ed090f96932bc6d211daef045a7a857eac4
V.C <seiri17725@gmail.com>
2021-01-31 15:23:40 +0900

    widened the scope of the sys insert, as well as added safety dir change in tests in case of pip install testing when tests open files

--- {{cookiecutter.repo_name}}/tests/context.py
+++ {{cookiecutter.repo_name}}/tests/context.py
@@ -1,5 +1,6 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../{{cookiecutter.pkg_name}}')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import {{cookiecutter.pkg_name}}
+
--- {{cookiecutter.repo_name}}/tests/{{cookiecutter.pkg_name}}_test.py
+++ {{cookiecutter.repo_name}}/tests/{{cookiecutter.pkg_name}}_test.py
@@ -1,5 +1,11 @@
+import os
 import pytest
 from .context import {{cookiecutter.pkg_name}}
+
+# the below two lines are for pip installing with test option and the tests will open files:
+CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
+os.chdir(CURRENT_DIR)
+
 
 def test_{{cookiecutter.pkg_name}}():
     assert 1 == 1
