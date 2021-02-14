a805907527ff0d6ad7c26285516efa40890db080
V.C <seiri17725@gmail.com>
2021-02-14 19:36:58 +0900

    revised comments and added requirements file template

--- {{cookiecutter.repo_name}}/tests/{{cookiecutter.pkg_name}}_test.py
+++ {{cookiecutter.repo_name}}/tests/{{cookiecutter.pkg_name}}_test.py
@@ -2,7 +2,8 @@ import os
 import pytest
 from .context import {{cookiecutter.pkg_name}}
 
-# the below two lines are for pip installing with test option and the tests will open files:
+# the below two lines are for pip installing with test option and when
+# the tests will open files:
 CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
 os.chdir(CURRENT_DIR)
 
